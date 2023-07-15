ecd45992fa1352d1374cb71e4af892eb656c488b
Rubens Filipe <rubensfilipe6@gmail.com>
2023-07-15 20:03:25 -0300

    ajustando index

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
 const jsonServer = require('json-server')
+const port = 3001 
+
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const port = 3001
 
 server.use(middlewares)
 server.use(router)
-server.listen(port)
+
+app.use(express.urlencoded({extended:true}))
+app.listen(port,() =>{
+	console.log('servidor rodando')
+})
