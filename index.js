a7b010dfb2effaba1804af9521183e52caf6d000
Rubens Filipe <rubensfilipe6@gmail.com>
2023-07-19 13:45:29 -0300

    add modificacoes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = 3001
-
+const router = jsonServer.router('db.json')
+//json-server --watch db.json --port 3001
 server.use(middlewares)
 server.use(router)
 server.listen(port)
